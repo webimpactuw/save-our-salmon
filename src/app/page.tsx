8ff2d0e151b810e31da6e80b3b597cda0d6841ea
YOUR FULLNAME <quintondpharr@gmail.com>
2024-02-07 12:48:40 -0800

    started on home page header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,17 @@ const Home = () => {
   return (
     <main className={styles.main}>
       <nav className={styles.nav}>
-        <div>SAVE OUR SALMON</div>
+        {/* Need to add actual logo instead of just text */}
+        <div>SAVE OUR SALMON</div> 
         <div className={styles.links}>
-          <Link href="/about">ABOUT</Link>
+          <Link href="/murals" className='with-dropdown'>Murals</Link>
+          <Link href="/events">Events</Link>
+          <Link href="/shop">Shop</Link>
+          <Link href="/about">About Us</Link>
+          <Link href="/resources">Resources</Link>
+          {/* Need to figure out socials icons: youtube, instagram, linkedin, facebook */}
+          <div className={styles.socials}>
+          </div>
         </div>
       </nav>
     </main>
