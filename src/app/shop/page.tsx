7b12553aad9fc136148877b17093fb106b3167db
FRCroboSam <samuelwang787@gmail.com>
2024-02-13 18:46:29 -0800

    Crusty Carousel Code

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,26 @@
-import styles from './page.module.scss'
 
-export default function Shop() {
-  return <div>About Shop</div>;
-}
+"use client";
+
+import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+export default function CarouselComp() {
+  return (
+    <>
+      <div className='max-w-[600px] mx-auto'>
+        <Carousel showArrows={true} autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false}>
+            <div>
+                <img src="https://picsum.photos/id/1018/1000/600/" alt="Banner image" width={1200} height={300}/>
+            </div>
+            <div>
+                <img src="https://picsum.photos/id/1015/1000/600/" alt="Banner image"  width={1200} height={300}/>
+            </div>
+            <div>
+                <img src="https://picsum.photos/id/1019/1000/600/"  alt="Banner image" width={1200} height={300} />
+            </div>
+        </Carousel>
+      </div>
+    </>
+  );
+};
