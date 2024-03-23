9a501c5801bb4efd1f6b7a313374b76a00a19677
FLinLan <maxlan0987@gmail.com>
2024-03-22 20:04:53 -0700

    added mural components to Juanita, McAleer, and USGS

--- src/app/murals/page.tsx
+++ src/app/murals/page.tsx
@@ -1,5 +1,22 @@
-import styles from './pages.module.scss';
+// MuralPage.tsx
+import React from 'react';
+import MuralHeader from './MuralHeader/MuralHeader';
+import MuralHero from './MuralHero/MuralHero'; // Assuming you have a Hero component as well
+import MuralCarousel from './MuralCarousel/MuralCarousel';
+import MuralMap from './MuralMap/MuralMap';
+import MuralFooter from './MuralFooter/MuralFooter';
+import styles from './MuralPage.module.scss';
 
-export default function MuralsHome () {
-    return <div>Murals Home</div> 
-}
+const MuralPage: React.FC = () => {
+  return (
+    <div className={styles.muralPage}>
+      <MuralHeader />
+      <MuralHero />
+      <MuralCarousel />
+      <MuralMap />
+      <MuralFooter />
+    </div>
+  );
+};
+
+export default MuralPage;
