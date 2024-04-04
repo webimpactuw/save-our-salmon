647009c812550dbe4c578b449fa1e79bb9b5f0ec
Andrew Zhou <andrewzhou16@gmail.com>
2024-04-04 12:32:10 -0700

    Add: Basic shop

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,26 +1,188 @@
-
-"use client";
-
-import Image from "next/image";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { UnstyledLink } from '../components/UnstyledLink';
+import styles from './page.module.scss';
 
 export default function CarouselComp() {
   return (
-    <>
-      <div className='max-w-[600px] mx-auto'>
-        <Carousel showArrows={true} autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false}>
-            <div>
-                <img src="https://picsum.photos/id/1018/1000/600/" alt="Banner image" width={1200} height={300}/>
-            </div>
-            <div>
-                <img src="https://picsum.photos/id/1015/1000/600/" alt="Banner image"  width={1200} height={300}/>
-            </div>
-            <div>
-                <img src="https://picsum.photos/id/1019/1000/600/"  alt="Banner image" width={1200} height={300} />
-            </div>
-        </Carousel>
+    <div className={styles.wrapper}>
+      <div className={styles.splash}>
+        <div className={styles.shade} />
+        <div className={styles.name}>SHOP</div>
+        <img src="/Shop Header.jpg" />
       </div>
-    </>
+      <div className={styles.waveTop1} />
+      <div className={styles.tagline}>
+        <div className={styles.title}>Purchase with a Purpose.</div>
+        <div className={styles.subtitle}>
+          100% of proceeds go to North Lake Washington SalmonWatchers –
+          supporting restoration in local streams.
+        </div>
+      </div>
+      <div className={styles.shopWrapper}>
+        <div className={styles.sectionWrapper}>
+          <div className={styles.sectionName}>
+            <img src="/Salmon B.png" />
+            <div className={styles.text}>Apparel</div>
+          </div>
+          <div className={styles.carousel}>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/2c8980/5313786270/il_640xN.5313786270_qrt6.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    McAleer Creek Save Our Salmon T-Shirt
+                  </div>
+                  <div className={styles.price}>$25.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/379ac7/5361960349/il_640xN.5361960349_5vcp.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>Save Our Salmon T-Shirt</div>
+                  <div className={styles.price}>$25.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/98fe8c/5361959507/il_640xN.5361959507_69g0.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>Great Blue Heron T-Shirt</div>
+                  <div className={styles.price}>$25.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/bc155b/5313715542/il_640xN.5313715542_m0ow.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>Great Blue Heron T-Shirt</div>
+                  <div className={styles.price}>$25.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/b9ed02/5313715314/il_640xN.5313715314_55ll.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    Cyclist T-shirt Biker in Blue Bicycle
+                  </div>
+                  <div className={styles.price}>$25.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+          </div>
+        </div>
+        <div className={styles.sectionWrapper}>
+          <div className={styles.sectionName}>
+            <img src="/Salmon B.png" />
+            <div className={styles.text}>Stickers & Magnets</div>
+          </div>
+          <div className={styles.carousel}>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/c/2284/1816/410/136/il/0ac6ba/4193317430/il_640xN.4193317430_o8i6.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    4x3.4" Sticker "Save Our Salmon"
+                  </div>
+                  <div className={styles.price}>$4.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/c/3000/2250/0/0/il/a4d80b/5438273396/il_640xN.5438273396_9upy.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    3.5" Sticker set "Sockeye Salmon"
+                  </div>
+                  <div className={styles.price}>$8.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/3b1569/3988398374/il_1588xN.3988398374_hee6.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    4.1x3.7" Sticker "Save Our Salmon"
+                  </div>
+                  <div className={styles.price}>$4.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/5a4e44/5717108145/il_1588xN.5717108145_a2nh.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    Set of 3 stickers- USGS (US Geological Survey) Western
+                    Fisheries Research Center
+                  </div>
+                  <div className={styles.price}>$10.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/e65d0a/5521526068/il_1588xN.5521526068_nn1d.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    4x2.7" Sticker "Steelhead Salmon" Rainbow Trout
+                  </div>
+                  <div className={styles.price}>$4.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/ab3899/3855302129/il_1588xN.3855302129_p5pd.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    3.2x2.9" Sticker "Heron Flying" - Great Blue Heron Wetland
+                    Art
+                  </div>
+                  <div className={styles.price}>$4.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/f00381/5521534020/il_1588xN.5521534020_iypo.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    3.7x2.7" Sticker "Scuba Diver" Research Diver - USGS Western
+                    Fisheries Research Center
+                  </div>
+                  <div className={styles.price}>$4.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+            <UnstyledLink href="/shop" target="_blank">
+              <div className={styles.item}>
+                <img src="https://i.etsystatic.com/22055441/r/il/0bbc6a/5521537736/il_1588xN.5521537736_qyhd.jpg" />
+                <div className={styles.info}>
+                  <div className={styles.name}>
+                    4x2.4" Sticker "Research Vessel" Boat - USGS Western
+                    Fisheries Research Center
+                  </div>
+                  <div className={styles.price}>$4.00</div>
+                </div>
+              </div>
+            </UnstyledLink>
+          </div>
+        </div>
+      </div>
+      <div className={styles.thanks}>
+        <div className={styles.title}>Thanks for Supporting!</div>
+        <div className={styles.subtitle}>
+          SOS merch has raised $4,500 (and counting!) to restore local salmon
+          streams.
+        </div>
+      </div>
+    </div>
   );
-};
+}
